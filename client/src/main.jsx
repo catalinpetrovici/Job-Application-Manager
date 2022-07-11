5c081ac685f9d0f685d19b9a492bc202bcbe61ea
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-07-11 14:06:05 +0300

    Global Context & Reducer & Display / Clear Alert

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 // import 'normalize';
 import './index.css';
+import { AppProvider } from '@context/appContext';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AppProvider>
+    <App />
+  </AppProvider>
+);
