5bef0959791483bfd2615fa3ed44cbd85dfc79b5
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-07-11 13:19:31 +0300

    Form Row - Register Page - name, email, password

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,55 @@
-const Register = () => {
-  return <div>Register</div>;
+import { useState, useEffect } from 'react';
+import { Logo, FormRow } from '@components';
+import Wrapper from '@assets/wrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
 };
 
+const Register = () => {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        {/* name input */}
+        <FormRow
+          type='text'
+          name='name'
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email input */}
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password input */}
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+      </form>
+    </Wrapper>
+  );
+};
 export default Register;
