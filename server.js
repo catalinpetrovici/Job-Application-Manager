29a0f57c1baa23a764998ce32f6e9b90958e13e1
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-07-11 14:46:12 +0300

    NotFoundMiddleware & ErrorHandlerMiddleware

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ import startServer from './utils/startServer.js';
 import express from 'express';
 const app = express();
 
+// middleware
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
 app.get('/', (req, res) => {
   res.send('Welcome!');
 });
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 startServer(app);
