3a14b0cf621d8d9a7adcfceb31ccbe89d750f19f
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-07-12 13:14:03 +0300

    Basic Register Controller & User Model with validate email

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,12 +1,19 @@
-const register = (req, res) => {
-  res.status(200).json({ msg: 'register' });
+import User from '../models/User.js';
+
+const register = async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(200).json({ user });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const login = (req, res) => {
+const login = async (req, res) => {
   res.status(200).json({ msg: 'login' });
 };
 
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
   res.status(200).json({ msg: 'updateUser' });
 };
 
