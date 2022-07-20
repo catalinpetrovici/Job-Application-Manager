925b967a13ad9448bcfd437f207d46fd050aea0e
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-07-20 13:28:23 +0300

    Pagination - Node.js

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -66,11 +66,17 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position');
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+  // 75 => 10 10 10 10 10 10 10 5
+
   const jobs = await result;
 
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const updateJob = async (req, res) => {
