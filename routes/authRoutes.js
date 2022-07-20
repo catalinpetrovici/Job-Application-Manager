109223aba13b00dbb5a0c6d6e6bb65577fd37381
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-07-20 14:49:47 +0300

    Security Packages - Express-rate-limiter only for register / login routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,17 @@ import express from 'express';
 const router = express.Router();
 import { register, login, updateUser } from '../controllers/authControllers.js';
 import authenticateUser from '../middleware/auth.js';
+import rateLimiter from 'express-rate-limit';
 
-router.post('/register', register);
-router.post('/login', login);
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message:
+    'Too many requests from this IP address, please try again after 15 minutes',
+});
+
+router.post('/register', apiLimiter, register);
+router.post('/login', apiLimiter, login);
 router.patch('/updateUser', authenticateUser, updateUser);
 
 export default router;
